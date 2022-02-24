474b6aab2b5bd0be24d905a33572bfe526e132d7
Roxy100 <misoamy16@gmail.com>
2022-02-24 17:51:15 +0900

    메뉴 추가하기 - 확인버튼 클릭이벤트 처리하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
 // - [X] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - [X] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [X] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [X] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [X] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -9,16 +10,11 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // form태그가 자동으로 전송되는 걸 막아준다.
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
-  // 메뉴의 이름을 입력받는건
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
-    if (e.key !== "Enter") {
-      return;
-    }
+  const addMenuName = () => {
     if ($("#espresso-menu-name").value === "") {
       alert("값을 입력해주세요.");
       return;
@@ -49,6 +45,17 @@ function App() {
     const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
     $(".menu-count").innerText = `총 ${menuCount}개`;
     $("#espresso-menu-name").value = "";
+  };
+
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addMenuName();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") {
+      return;
+    }
+    addMenuName();
   });
 }
 
